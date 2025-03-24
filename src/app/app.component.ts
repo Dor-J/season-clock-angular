b25edcabd8c3d81c897ea52030f71db66e6ce743
Dor-J <Mr.DorJosef@gmail.com>
2025-03-24 18:27:18 +0200

    Implement season clock component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { seasonClockService } from '../services/season-clock.service';
+import { TimeObj } from './models/time-obj.model';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,31 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  onToggleTheme() {
-    throw new Error('Method not implemented.');
-  }
+export class AppComponent implements OnInit, OnDestroy {
   title = 'season-clock-angular';
+  isDark: boolean = false;
+
+  timeObj: TimeObj = {
+    season: 'Winter',
+    month: 'January',
+    dayOfWeek: 'Thursday',
+  };
+
+  async ngOnInit() {
+    console.log('on init called');
+    try {
+      const seasonData: TimeObj = (await seasonClockService.query()) as TimeObj;
+      if (seasonData) {
+        this.timeObj = seasonData;
+      }
+    } catch (error) {
+      console.error('error fetching data:', error);
+    }
+  }
+
+  ngOnDestroy() {}
+
+  onToggleTheme() {
+    this.isDark = !this.isDark;
+  }
 }
