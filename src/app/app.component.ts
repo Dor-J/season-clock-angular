627833697d32fd0ef2b38f37186ed8367c381f4e
Dor-J <Mr.DorJosef@gmail.com>
2025-03-24 18:06:44 +0200

    Add onToggleTheme func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  onToggleTheme() {
+    throw new Error('Method not implemented.');
+  }
   title = 'season-clock-angular';
 }
